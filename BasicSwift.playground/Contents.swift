3013b346b0816d25996f0e7b24b622b17fe579ab
David Jardon <d.jardon@gmail.com>
2021-01-13 09:46:53 +0100

    variables and constants

---
+++ BasicSwift.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// ************* Var / Const *************
+
+// Variable: var nombreVariable: Tipo = valor
+var str = "Hello, playground"
+str = "Hola, playground"
+
+// Constante: let nombreConstante: Tipo = valor
+let constant = "Hola, playground"
+// Error -> constant = "Hello, playground"
+
+var variable = str
+variable = "Hola bootcamp!"
+str = "Hola David!"
