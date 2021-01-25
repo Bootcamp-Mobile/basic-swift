6ace91883819610e084683d4d89c87bfcd03b5fe
David jardón <d.jardon@gmail.com>
2021-01-25 12:56:58 +0100

    Extensions, Protocols and Inheritance

--- BasicSwift.playground/Contents.swift
+++ BasicSwift.playground/Contents.swift
@@ -835,11 +835,6 @@ class Student {
     var address: String?
     var email: String
     var phone: String?
-    
-    // Computed property
-    var fullName: String {
-        return "\(name) \(lastname)"
-    }
 
     // Constructor o inicializador de la clase Student
     init(name: String = "", lastname: String = "", email: String = "", age: Int? = nil, address: String = "", phone: String = "") {
@@ -902,7 +897,6 @@ studentList.append(contentsOf: [student,
 studentList[0].fullName
 
 
-print("****** 9 ******")
 // Crear una lista con los datos de los estudiantes del Bootcamp
 let bootcampStudents = [Student(name: "Elena",
                                 lastname: "Barbullushi",
@@ -935,55 +929,6 @@ let bootcampStudents = [Student(name: "Elena",
                                 email: "a.rio@email.com",
                                 age: 29)]
 
-print("****** 9.1 ******")
-// Escribir en consola los nombres de los estudiantes con menos de 30 años
-for student in bootcampStudents {
-    // Desempaquetar de forma segura 'student.age' que es opcional 'Int?',
-    // después comprobamos si 'age' es menor que 30
-    if let age = student.age, age < 30 {
-        print("\(student.fullName) age \(age)")
-    }
-    
-    /*
-    // Podríamos utilizar un guard-let en lugar del if-let anterior
-    guard let age = student.age,
-          age < 30 else {
-        // Si NO se cumplen las condiciones salto
-        // a la siguiente iteración con 'continue'
-        continue
-    }
-    
-    print("\(student.fullName()) age \(age)")
-    */
-}
-
-// Mismo ejemplo utilizando el closure 'forEach'
-bootcampStudents.forEach { student in
-    if let age = student.age, age < 30 {
-        print(student.fullName)
-    }
-}
-
-print("****** 9.2 ******")
-// Escribir en consola los nombres de los estudiantes con menos de 30 años
-// y su email tenga más de 15 letras
-bootcampStudents.forEach { student in
-    if let age = student.age, age < 30, student.email.count > 15 {
-        print("\(student.fullName) age \(age) email \(student.email)")
-    }
-}
-
-
-print("****** 9.3 ******")
-// Escribir en consola los nombres de los estudiantes con más de 30 años
-// y que tengan número de teléfono
-bootcampStudents.forEach { student in
-    if let phone = student.phone, let age = student.age, age < 30, !phone.isEmpty {
-        print("\(student.fullName) age \(age) phone \(phone)")
-    }
-}
-
-
 class Teacher: CustomStringConvertible {
     var name: String
     var lastname: String
@@ -1001,10 +946,6 @@ class Teacher: CustomStringConvertible {
             "age": \(age ?? -1)
         }
         """
-    }
-    
-    var fullName: String {
-        return "\(name) \(lastname)"
     }
     
     init(name: String = "", lastname: String = "", email: String = "", age: Int? = nil) {
@@ -1105,435 +1046,160 @@ let bootcamps = [Bootcamp(name: "Bootcamp Mobile",
                           teachers: bootcampTeachers.filter { $0.name.contains("o") },
                           students: bootcampStudents.filter { $0.name.contains("i") })]
 
-print("****** 10 ******")
-// Escribir en consola, por cada bootcamp,
-// el nombre de cada estudiante del bootcamp
-for bootcamp in bootcamps {
-    print("\(bootcamp.name), \(bootcamp.studentsCount) estudiantes:")
-    for student in bootcamp.students {
-        print(student.name)
-    }
-    print()
-}
-
-print()
-print("****** 10.1 ******")
-// Escribir en consola, por cada bootcamp,
-// el nombre de cada profesor del bootcamp
-for bootcamp in bootcamps {
-    print("\(bootcamp.name), \(bootcamp.teachersCount) profesores:")
-    for teacher in bootcamp.teachers {
-        print(teacher.name)
-    }
-    print()
-}
 
 
-print()
-print("****** 10.2 ******")
-// Escribir en consola el nombre de los bootcamps que tengan más de 2 estudiantes
-/*
- for bootcamp in bootcamps {
-    if(bootcamp.students.count > 2) {
-        print(bootcamp.name)
-    }
- }
- 
- bootcamps.forEach { bootcamp in
-     if(bootcamp.students.count > 2) {
-         print(bootcamp.name)
-     }
- }
- */
-for bootcamp in bootcamps where bootcamp.students.count > 2 {
-    print(bootcamp.name)
-}
-
-
-print()
-print("****** 10.3 ******")
-// Escribir en consola el nombre de los bootcamps que tengan más de 1 profesor
-/*
- for bootcamp in bootcamps {
-    if(bootcamp.teachers.count > 1) {
-        print(bootcamp.name)
-    }
- }
- 
- bootcamps.forEach { bootcamp in
-     if(bootcamp.teachers.count > 1) {
-         print(bootcamp.name)
-     }
- }
- */
-for bootcamp in bootcamps where bootcamp.teachers.count > 1 {
-    print(bootcamp.name)
-}
-
-
-print()
-print("****** 10.4 ******")
-// Escribir en consola el nombre de todos los estudiantes de los bootcamps sin repetir nombres
-var studentNames: Set<String> = []
-for bootcamp in bootcamps {
-    for student in bootcamp.students {
-        studentNames.insert(student.name)
-    }
-}
-print(studentNames)
-
-
-print()
-print("****** 10.5 ******")
-// Escribir en consola el nombre de todos los profesores de los bootcamps sin repetir nombres
-var teacherNames: Set<String> = []
-for bootcamp in bootcamps {
-    for teacher in bootcamp.teachers {
-        teacherNames.insert(teacher.name)
-    }
-}
-print(teacherNames)
-
-
-print()
-print("****** 10.6 ******")
-// Escribir en consola el nombre de todos los alumnos de los bootcamps sin repetir nombres ordenados alfabeticamente
-var studentNamesSorted: Set<String> = []
-for bootcamp in bootcamps {
-    for student in bootcamp.students {
-        studentNamesSorted.insert(student.name)
-    }
-}
-// print(studentNamesSorted.sorted())
-print(studentNamesSorted.sorted(by: { (name1, name2) -> Bool in
-    return name1.compare(name2) == .orderedAscending
-}))
-
-
-print()
-print("****** 10.7 ******")
-// Escribir en consola el nombre de todos los profesores de los bootcamps sin repetir nombres ordenados alfabeticamente
-var teacherNamesSorted: Set<String> = []
-for bootcamp in bootcamps {
-    for teacher in bootcamp.teachers {
-        teacherNamesSorted.insert(teacher.name)
-    }
-}
-print(teacherNamesSorted.sorted())
-
-
-print()
-print("****** 10.8 ******")
-// Escribir en consola el nombre de los estudiantes que están en más de un bootcamp
-/* Ejemplo
- Alumno Belén 3 bootcamps
- Alumno Miguel 2 bootcamps
- */
-var studentBootcamps: [String:Int] = [:]
-bootcamps.forEach { bootcamp in
-    bootcamp.students.forEach { student in
-        studentBootcamps[student.name] = (studentBootcamps[student.name] ?? 0) + 1
-    }
-}
-
-for (name, bootcamps) in studentBootcamps where bootcamps > 1 {
-    print("Alumno \(name) \(bootcamps) bootcamps")
-}
-
-/* Ejemplo compactMap
- let studentsMore1Bootcamp: [String] = bootcampStudents.compactMap { ($0.name.count > 5) ? $0.name : nil }
-
- let studentsMore1Bootcamp: [String] = bootcampStudents.compactMap { student in
-    if(student.name.count > 5) {
-        return student.name
-    } else {
-        return nil
-    }
- }
-
-// Operador ternario es una simplificación de un if: (condición) ? valorDevuelto_SiCondiciónTrue : valorDevuelto_SiCondiciónFalse
-
-let studentsMore1Bootcamp: [String] = bootcampStudents.compactMap { student in
-    let studentName: String? = (student.name.count > 5) ? student.name : nil
-    return studentName
-}
- */
-
-
-/* Ejemplo filter
-let bootcamps5: [Bootcamp] = bootcamps.filter { $0.name.count > 5}
-let bootcamps5: [Bootcamp] = bootcamps.filter { bootcamp in
-    return bootcamp.name.count > 5
-}
-*/
-
-/* Ejemplo contains
-var studentToSearch = "Elena"
-let existsStudent: Bool = bootcampStudents.contains { $0.name.compare(studentToSearch) == .orderedSame }
+// ************* Extensions *************
+// Declaramos una extensión de la clase Student
+extension Student {
     
-let existsStudent: Bool = bootcampStudents.contains { student -> Bool in
-    return student.name.compare(studentToSearch) == .orderedSame
-}
-*/
+    // En una extension no podemos añadir stored properties
+    // var age: Int
 
-
-/*
- let studentsMore1Bootcamp: [String] = bootcampStudents.compactMap { student in
-     return (bootcamps.filter { $0.students.contains { $0.name.compare(student.name) == .orderedSame } }.count > 1 ? student.name : nil)
- }
- print(studentsMore1Bootcamp)
- */
-
-/*
- bootcampStudents.forEach { student in
-     let studentBootcamps = bootcamps.filter { bootcamp in
-         // return filter
-         return bootcamp.students.contains { bootcampStudent in
-             // return contains
-             return bootcampStudent.name.compare(student.name) == .orderedSame
-         }
-     }
-     
-     if (studentBootcamps.count > 1) {
-         print("Alumno \(student.name) \(studentBootcamps.count) bootcamps")
-     }
- }
- */
-
-/*
- Podemos utilizar en el closure: $0.age != nil ? $0:nil
-let studentsWithAge: [Student] = bootcampStudents.compactMap { student in
-    if(student.age != nil) {
-        return student
-    } else {
-        return nil
+    // Computed property
+    var fullNameCount: Int {
+        return fullName.count
     }
 }
- */
 
-let studentsMore1Bootcamp: [(name: String, count: Int)] = bootcampStudents.compactMap { student in
-    // Filter devuelve una lista con los valores de bootcamps que cumplen la condición
-    let studentBootcamps = bootcamps.filter { bootcamp in
-        // return filter
-        return bootcamp.students.contains { bootcampStudent in
-            // return contains
-            return bootcampStudent.name.compare(student.name) == .orderedSame
+extension Student: CustomStringConvertible {
+    var description: String {
+        return "Student \(fullName)"
+    }
+}
+
+print(student)
+
+
+// ************* Protocol *************
+protocol PersonName {
+    var fullName: String { get set }
+    
+    func isEqualTo(other: String) -> Bool
+}
+
+
+// Hacemos que la clase Teacher conforme el protocolo PersonName
+// esto obliga a implementar la variable 'fullName' y la función 'isEqualTo()'
+extension Teacher: PersonName {
+    var fullName: String {
+        get {
+            return "\(name) \(lastname)"
+        }
+        set {
+            // 'newValue' es el nuevo valor que se ha asignado a la variable
+            name = newValue.components(separatedBy: " ").first ?? ""
+            lastname = newValue.components(separatedBy: " ").last ?? ""
         }
     }
     
-    // return CompactMap
-    return (studentBootcamps.count > 1 ? (student.name, studentBootcamps.count) : nil)
+    func isEqualTo(other: String) -> Bool {
+        return name.compare(other) == .orderedSame
+    }
 }
 
-studentsMore1Bootcamp.forEach { print("Alumno \($0.name) \($0.count) bootcamps") }
-
-
-print()
-print("****** 10.9 ******")
-// Escribir en consola el nombre de los profesores que están en más de un bootcamp
-/* Ejemplo
- Profesor David 3 bootcamps
- Profesor Sara 2 bootcamps
- */
-let teachersMore1Bootcamp: [(name: String, count: Int)] = bootcampTeachers.compactMap { teacher in
-    let teacherBootcamps = bootcamps.filter { $0.teachers.contains { $0.name.compare(teacher.name) == .orderedSame } }
-    return teacherBootcamps.count > 1 ? (teacher.name, teacherBootcamps.count) : nil
-}
-teachersMore1Bootcamp.forEach { print("Profesor \($0.name) \($0.count) bootcamps") }
-
-
-print()
-print("****** 10.10 ******")
-// Escribir en consola el nombre de cada estudiante y los de sus profesores
-/* Ejemplo
- Alumno Belén, 2 profesores:
- David
- Sara
- */
-
-/// Filtra la lista de **bootcamps** recibida en base al nombre del **student** y devuelve los bootcamps
-/// que contengan al **student**
-/// - Parameters:
-///   - bootcamps: listado de bootcamps
-///   - student: estudiante a buscar en cada bootcamp
-/// - Returns: Listado de **bootcamps** filtrados
-func filter(bootcamps: [Bootcamp], by student: Student) -> [Bootcamp] {
-    return bootcamps.filter { contains(bootcamp: $0, student: student) }
-}
-
-func contains(bootcamp: Bootcamp, student: Student) -> Bool {
-    return bootcamp.students.contains { areStudentNameEquals(student: $0, other: student) }
-}
-
-func areStudentNameEquals(student: Student, other: Student) -> Bool {
-    return student.name.compare(other.name) == .orderedSame
-}
-
-let studentsTeachersBootcamp: [(name: String, bootcamps: [Bootcamp])] = bootcampStudents.compactMap { student in
-    // Filter devuelve una lista con los valores de bootcamps que cumplen la condición
-    let studentBootcamps = filter(bootcamps: bootcamps, by: student)
+// Hacemos que la clase Student conforme el protocolo PersonName
+// esto obliga a implementar la variable 'fullName' y la función 'isEqualTo()'
+extension Student: PersonName {
+    var fullName: String {
+        get {
+            return "\(name) \(lastname)"
+        }
+        set {
+            // 'newValue' es el nuevo valor que se ha asignado a la variable
+            name = newValue.components(separatedBy: " ").first ?? ""
+            lastname = newValue.components(separatedBy: " ").last ?? ""
+        }
+    }
     
-    // return CompactMap
-    return (studentBootcamps.count > 1 ? (student.name, studentBootcamps) : nil)
+    func isEqualTo(other: String) -> Bool {
+        return name.compare(other) == .orderedSame
+    }
 }
 
-studentsTeachersBootcamp.forEach { student in
-    var teachers: Set<String> = []
-    student.bootcamps.forEach { $0.teachers.forEach { teachers.insert($0.name) } }
+// 'student' y 'teacherDavid' son variable que ya hemos declarado
+// Como las dos clases conforman el protocolo PersonName, las podemos
+// añadir al mismo listado de datos
+var persons: [PersonName] = [student, teacherDavid]
+var person = persons.first
+
+person?.isEqualTo(other: "")
+
+student.fullName
+student.fullName = "Natalia Soria"
+student.fullName
+student.name
+student.lastname
+
+
+// ************* Inheritance *************
+
+class Vehiculo {
+    var modelo: String
+    var velocidad: Int = 0
+    var ruedas: Int = 0
     
-    print("Alumno \(student.name), \(teachers.count) profesores:")
-    teachers.forEach { print($0) }
+    init(modelo: String) {
+        self.modelo = modelo
+    }
+    
+    func acelerar() {
+        velocidad += 10
+    }
+    
+    func frenar() {
+        if(velocidad >= 10) {
+            velocidad -= 10
+        }
+    }
 }
 
-print()
-print("****** 10.11 ******")
-// Escribir en consola el nombre de cada profesor y los de sus alumnos
+class Coche: Vehiculo {
+    var numPuertas: Int = 4
+    
+    // Sobreescribímos la funcionalidad de acelerar()
+    override func acelerar() {
+        velocidad += 30
+    }
+    
+    override func frenar() {
+        if(velocidad > 30) {
+            // Llamamos a la función frenar() de la clase padre
+            // de Vehículo
+            super.frenar()
+        }
+    }
+}
 
-// OBJETIVO: Conseguir la lista de bootcamps de cada profesor
-
-// 1.- Ver cual es la lista de profesores
-// bootcampsTeacher
-// 2.- Ir uno a uno por cada profesor de la lista
-// Ejemplo: profesor = "Juan"
-// 3.- Ver cual es la lista de bootcamps
-// bootcamps
-// 4.- Comprobar cuales son los bootcamps de cada profesor
-// 4.1.- Ir uno a uno por cada bootcamp
-// 4.1.1.- Comprobar si mi profesor está en la lista de profesores del bootcamp
-// 4.1.2- Ir uno a uno por cada profesor del bootcamp
-// 4.1.3.- Comprobar si cada profesor del bootcamp es igual a mi profesor ("Juan")
-// 4.1.3.1.- Comparar si el nombre del profesor del bootcamp es igual al de mi profesor ("Juan")
-
-// RESULTADO: Lista de bootcamps de cada profesor
-// Ejemplo: teacherBootcampList
+class Moto: Vehiculo {
+    var cilindrada: Int = 1000
+    
+    init(modelo: String, ruedas: Int) {
+        // En el init del hijo siempre tendremos que llamar
+        // al init del padre con super.init()
+        super.init(modelo: modelo)
+        
+        self.ruedas = ruedas
+    }
+}
 
 
-// OBJETIVO: Conseguir la lista de alumnos de cada bootcamp de cada profesor
-
-// 5.- Ver cual es la lista de todos los estudiantes
-// bootcampsStudents
-// 6.- Ir uno a uno por cada estudiante de la lista
-// 6.1.- Comprobar si mi alumno existe en la lista 'teacherBootcampList'
-// 6.1.1.- Ir uno a uno por cada bootcamp de 'teacherBootcampList'
-// 6.1.1.1.- Comprobar si mi estudiante está en la lista de estudiantes del bootcamp
-// 6.1.1.2.- Ir uno a uno por la lista de estudiantes del bootcamp
-// 6.1.1.2.1- Comprobar si el alumno del bootcamp es igual a mi estudiante
-// 6.1.1.2.2- Comparar si el nombre del alumno del bootcamp es igual al nombre del estudiante
-
-// RESULTADO: Lista de alumnos del profesor
+class Bicicleta: Vehiculo {
+    var isCascoDisponible: Bool = false
+    
+}
 
 
 
+var peugeot = Coche(modelo: "Peugeot")
+peugeot.numPuertas = 5
+peugeot.ruedas = 4
 
+peugeot.acelerar()
+peugeot.velocidad
+peugeot.frenar()
 
+var honda = Moto(modelo: "Honda",
+                 ruedas: 2)
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* Ejemplo
- Profesor David, 3 alumnos:
- Belén
- Miguel
- Alex
- */
-
-
-print()
-print("****** 10.12 ******")
-// Escribir en consola el nombre de cada alumno ordenados por la edad del alumno
-/* Ejemplo
- Alumno Sergi, edad 35
- Alumno Belén, edad 20
- ....
- */
-
-print()
-print("****** 10.13 ******")
-// Escribir en consola el nombre de cada profesor ordenados por la edad del profesor
-/* Ejemplo
- Profesor David, edad 35
- Profesor Carlos, edad 20
- ....
- */
-
-print()
-print("****** 10.14 ******")
-// Escribir en consola el nombre del alumno con mayor edad de cada bootcamp
-/* Ejemplo
- Bootcamp Mobile, alumno Sergi, edad 35
- Bootcamp Fullstack, alumno Elena, edad 27
- ....
- */
-
-print()
-print("****** 10.15 ******")
-// Escribir en consola el nombre del alumno con menor edad de cada bootcamp
-/* Ejemplo
- Bootcamp Mobile, alumno Sergi, edad 18
- Bootcamp Fullstack, alumno Elena, edad 21
- ....
- */
-
-print()
-print("****** 10.16 ******")
-// Escribir en consola el nombre de los profesores de cada bootcamp que sean mayores
-// que todos sus alumnos
-/* Ejemplo
- Bootcamp Mobile, profesor David, edad 35
- ....
- */
-
-print()
-print("****** 10.17 ******")
-// Escribir en consola el nombre de los profesores de cada bootcamp que sean menores
-// que todos sus alumnos
-/* Ejemplo
- Bootcamp Mobile, profesor David, edad 20
- ....
- */
-
-
-print()
-print("****** 10.18 ******")
-// Escribir en consola el nombre de cada alumno, el número de sus bootcamps y el nombre de cada bootcamp
-/* Ejemplo
- Alumno Alex, 2 bootcamps
- Bootcamps:
- Mobile
- Fullstack
- ....
- */
-
-
-print()
-print("****** 10.19 ******")
-// Escribir en consola el nombre de cada profesor, el número de sus bootcamps y el nombre de cada bootcamp
-/* Ejemplo
- Profesor David, 2 bootcamps
- Bootcamps:
- Mobile
- Fullstack
- ....
- */
-
-
-print()
-print("****** 10.20 ******")
-// Escribir en consola el nombre de cada alumno, el número de sus bootcamps, el nombre de cada bootcamp,
-// y el nombre del profesor de cada bootmcap
-/* Ejemplo
- Alumno Adrián, 2 bootcamps
- Bootcamps:
- Mobile, profesor David
- Fullstack, profesor Carlos
- ....
- */
+honda.acelerar()
+honda.frenar()
